cc7e9b88b27867c9ef9e9f278c8c805f670bab8e
abdellah-ch <abdellahchotta55@gmail.com>
2024-03-13 19:39:15 +0000

    re

--- lib/GetCalorieGoal.ts
+++ lib/GetCalorieGoal.ts
@@ -4,11 +4,11 @@ import {db} from "./prisma"
 
 export const GetCalorieGoal = async (userId:String) =>{
   
- const {daily} = await db.dailyCalGoal.findUnique({
+ const res = await db.dailyCalGoal.findUnique({
         where: {
           userId:userId
         },
     });
-return daily;
+return res?.daily;
 }
     
